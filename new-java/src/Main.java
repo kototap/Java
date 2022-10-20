5d800d6fe484f4057818aabcb544aa0639f70844
Kototap <kotonesmz31@gmail.com>
2022-10-20 10:30:52 +0900

    1-2

--- new-java/src/Main.java
+++ new-java/src/Main.java
@@ -6,6 +6,9 @@ public class Main {
   public static void main(String[] args) {
     int age;
     age = 30;
-    System.out.println(age);
+    System.out.println("私の年齢は" + age);
+    // 再代入
+    age = 35;
+    System.out.println("...いや本当は" + age + "歳");
   }
 }
