14e7e2264126cc0486a5f31b47d32f38bf78e3e2
Kototap <kotonesmz31@gmail.com>
2022-10-20 10:33:21 +0900

    1-3

--- new-java/src/Main.java
+++ new-java/src/Main.java
@@ -3,12 +3,23 @@ public class Main {
   //   System.out.println("すがわら");
 
   // } 
+  // public static void main(String[] args) {
+  //   int age;
+  //   age = 30;
+  //   System.out.println("私の年齢は" + age);
+  //   // 再代入
+  //   age = 35;
+  //   System.out.println("...いや本当は" + age + "歳");
+  // }
+
   public static void main(String[] args) {
-    int age;
-    age = 30;
-    System.out.println("私の年齢は" + age);
-    // 再代入
-    age = 35;
-    System.out.println("...いや本当は" + age + "歳");
+    double pi = 3.14;
+    int pie = 5;
+    System.out.println("半径" + pie + "cmのパイの面積は、");
+    System.out.println(pie * pie * pi);
+    System.out.println("パイの半径を倍にします");
+    pie = 10;
+    System.out.println("半径" + pie + "cmのパイの面積は、");
+    System.out.println(pie * pie * pi);
   }
 }
