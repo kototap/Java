36e162effbe487397601ae27c0e0c96939067ef6
Kototap <kotonesmz31@gmail.com>
2022-10-21 19:20:17 +0900

    MyDiary

---
+++ new-java/src/MyDiary.java
@@ -0,0 +1,10 @@
+public class MyDiary {
+  public static void main (String[] args){
+    int version;
+    version = 1 + 5;
+
+    System.out.println("日記システム");
+    System.out.println("vre" + version);
+    System.out.println("終了します。");
+  }
+}
