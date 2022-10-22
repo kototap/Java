818f9b7cf726d1d4a5e8bfbf3d7350c9ad99eca1
Kototap <kotonesmz31@gmail.com>
2022-10-22 23:52:44 +0900

    インプット

--- new-java/src/Main.java
+++ new-java/src/Main.java
@@ -54,10 +54,19 @@ public class Main {
   //     ("あなたは来年" + (n + 1) + "歳になりますね");
   // }
 
-    public static void main(String[] args){
-      int r = new java.util.Random().nextInt(90); //乱数を生み出して代入
+    // public static void main(String[] args){
+    //   int r = new java.util.Random().nextInt(90); //乱数を生み出して代入
 
-      System.out.println("あなたは多分" + r + "歳ですね");
+    //   System.out.println("あなたは多分" + r + "歳ですね");
+    // }
+
+    public static void main(String[] args){
+      System.out.println("あなたの名前を入力してください。");
+      String name = new java.util.Scanner(System.in).nextLine();
+      System.out.println("あなたの年齢を入力してください。");
+      int age = new java.util.Scanner(System.in).nextInt();
+      System.out.println
+        ("ようこそ、" + age + "歳の" + name + "さん。");
     }
 
 }
