e856d53e78bcd085e4446e4b5a4936fb8d98876f
Kototap <kotonesmz31@gmail.com>
2022-10-22 17:27:50 +0900

    practice1-2

---
+++ new-java/src/Pra1.java
@@ -0,0 +1,9 @@
+public class Pra1 {
+  public static void main(String[] args) {
+    int hight = 3;
+    int width = 5;
+    int area = hight * width;
+
+    System.out.println("縦幅" + hight + "横幅" + width + "の長方形の面積は" + area);
+  }
+}
