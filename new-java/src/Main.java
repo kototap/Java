17264c8572caefd7c70691b8c6cb5837433decba
Kototap <kotonesmz31@gmail.com>
2022-10-23 00:18:20 +0900

    占いプログラム

--- new-java/src/Main.java
+++ new-java/src/Main.java
@@ -69,11 +69,28 @@ public class Main {
     //     ("ようこそ、" + age + "歳の" + name + "さん。");
     // }
 
-      public static void main(String[] args){
-        int x = 5;
-        int y = 10;
+      // public static void main(String[] args){
+      //   int x = 5;
+      //   int y = 10;
 
-        String ans = "x + y は" + (x + y); //()を使う
-        System.out.println(ans);
+      //   String ans = "x + y は" + (x + y); //()を使う
+      //   System.out.println(ans);
+      // }
+
+
+      public static void main(String[] args){
+        System.out.println("ようこそ占いの館へ");
+        System.out.println("あなたの名前を入力してください。");
+        String name = new java.util.Scanner(System.in).nextLine();
+        System.out.println("あなたの年齢を入力してください。");
+        String ageString = new java.util.Scanner(System.in).nextLine();
+        int age = Integer.parseInt(ageString);
+        int fortune = new java.util.Random().nextInt(3);
+        fortune++;
+        System.out.println("占いの結果が出ました。");
+        System.out.println
+          (age + "歳の" + name + "さんの運気番号は" + fortune + "です。");
+        System.out.println("1:大吉 2:中吉 3:吉 4:凶");
+
       }
 }
