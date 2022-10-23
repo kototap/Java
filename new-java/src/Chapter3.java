58542d18c323bcedf78925d41cec5374ebe0f448
Kototap <kotonesmz31@gmail.com>
2022-10-23 21:40:40 +0900

    while

--- new-java/src/Chapter3.java
+++ new-java/src/Chapter3.java
@@ -9,21 +9,30 @@ public class Chapter3 {
   //   }
   // }
 
+  // public static void main(String[] args){
+  //   System.out.println("あなたの運勢を占います。");
+  //   int fortune = new java.util.Random().nextInt(4) + 1;
+  //   switch(fortune){
+  //     case 1:
+  //       System.out.println("大吉");
+  //       break;
+  //     case 2:
+  //       System.out.println("中吉");
+  //       break;
+  //     case 3:
+  //       System.out.println("吉");
+  //       break;
+  //     default:
+  //       System.out.println("小吉");
+  //   }
+  // }
+
   public static void main(String[] args){
-    System.out.println("あなたの運勢を占います。");
-    int fortune = new java.util.Random().nextInt(4) + 1;
-    switch(fortune){
-      case 1:
-        System.out.println("大吉");
-        break;
-      case 2:
-        System.out.println("中吉");
-        break;
-      case 3:
-        System.out.println("吉");
-        break;
-      default:
-        System.out.println("小吉");
+    int temp = new java.util.Random().nextInt(30) + 1;
+    System.out.println(temp);
+    while (temp > 25){
+      temp--;
+      System.out.println("現在の温度は" + temp + "度です。");
     }
   }
 
