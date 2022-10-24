6ce92cf0483f9223b105f039d2652af88c0803bf
Kototap <kotonesmz31@gmail.com>
2022-10-24 23:33:50 +0900

    Chapter4-2

--- new-java/src/Chapter3.java
+++ new-java/src/Chapter3.java
@@ -69,7 +69,7 @@ public class Chapter3 {
   //   }
   // }
 
-  
+
   //練習問題3-3
   // public static void main(String[] args){
   //   int isHungry = new java.util.Random().nextInt(2); //2以下の数字でランダム・・・０または１
@@ -116,23 +116,23 @@ public class Chapter3 {
   // }
 
 
-  //練習問題3-6
-  public static void main(String[] args){
-    System.out.println("【数当てゲーム】");
-    int ans = new java.util.Random().nextInt(10);
+  // //練習問題3-6
+  // public static void main(String[] args){
+  //   System.out.println("【数当てゲーム】");
+  //   int ans = new java.util.Random().nextInt(10);
     
-    for(int i = 0; i < 5; i++){
-      System.out.println("0~9の数字を入力してください。");
-      int num = new java.util.Scanner(System.in).nextInt();
-      if(num == ans){
-        System.out.println("あたり！");
-        break;
-      } else {
-        System.out.println("ハズレ！");
-      }
-    }
+  //   for(int i = 0; i < 5; i++){
+  //     System.out.println("0~9の数字を入力してください。");
+  //     int num = new java.util.Scanner(System.in).nextInt();
+  //     if(num == ans){
+  //       System.out.println("あたり！");
+  //       break;
+  //     } else {
+  //       System.out.println("ハズレ！");
+  //     }
+  //   }
 
-    System.out.println("ゲームを終了します。");
-  }
+  //   System.out.println("ゲームを終了します。");
+  // }
 
 }
---
+++ new-java/src/Chapter4.java
@@ -0,0 +1,7 @@
+public class Chapter4 {
+  public static void main(String[] args){
+    int[] scores = new int[7]; //int型の要素を5個作りscoresに代入する
+    int num = scores.length; //配列変数名.length -> 配列の要素数の取得
+    System.out.println("要素の数は" + num + "です。");
+  } 
+}
