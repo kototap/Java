adc51e5989f2322a640babd2f4b60706aef2d930
Kototap <kotonesmz31@gmail.com>
2022-10-24 09:22:06 +0900

    break-continue

--- new-java/src/Chapter3.java
+++ new-java/src/Chapter3.java
@@ -49,13 +49,23 @@ public class Chapter3 {
   // }
 
 
+  // public static void main(String[] args){
+  //   for (int i = 1; i < 10; i++) {
+  //     for (int j = 1; j < 10; j++) {
+  //       System.out.println(i * j);
+  //       // System.out.println("");
+  //     }
+  //     System.out.println(""); //改行
+  //   }
+  // }
+
   public static void main(String[] args){
-    for (int i = 1; i < 10; i++) {
-      for (int j = 1; j < 10; j++) {
-        System.out.println(i * j);
-        // System.out.println("");
+    for(int i = 1; i < 10; i++){
+      if(i == 3){
+        // break;
+        continue;
       }
-      System.out.println(""); //改行
+      System.out.println(i);
     }
   }
 }
