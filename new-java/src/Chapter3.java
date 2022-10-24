aab303c3ccb1900cbed8d2aeea121587a50aa585
Kototap <kotonesmz31@gmail.com>
2022-10-24 15:56:25 +0900

    Pra3-4

--- new-java/src/Chapter3.java
+++ new-java/src/Chapter3.java
@@ -69,22 +69,46 @@ public class Chapter3 {
   //   }
   // }
 
-//   public static void main(String[] args){
-//     int isHungry = new java.util.Random().nextInt(2); //2以下の数字でランダム・・・０または１
-//     String  food = "おにぎり";
+  // public static void main(String[] args){
+  //   int isHungry = new java.util.Random().nextInt(2); //2以下の数字でランダム・・・０または１
+  //   String  food = "おにぎり";
 
-//     System.out.println("こんにちは!");
+  //   System.out.println("こんにちは!");
     
-//     if (isHungry == 0){
-//       System.out.println("お腹いっぱいです。");
-//     } else {
-//       System.out.println("腹ペコです。");
-//     }
+  //   if (isHungry == 0){
+  //     System.out.println("お腹いっぱいです。");
+  //   } else {
+  //     System.out.println("腹ペコです。");
+  //   }
 
-//     if (isHungry == 1){
-//       System.out.println(food + "を食べます。");
-//       System.out.println("ごちそうさまでした！");
-//     }
+  //   if (isHungry == 1){
+  //     System.out.println(food + "を食べます。");
+  //     System.out.println("ごちそうさまでした！");
+  //   }
 
-//   }
-// }
+  // }
+
+  public static void main(String[] args){
+    System.out.println("[メニュー] 1:検索 2:登録 3:削除 4:変更 >");
+    // String selectedString = new java.util.Scanner(System.in).nextLine();
+    // int selected = Integer.parseInt(selectedString);
+    int selected = new java.util.Scanner(System.in).nextInt();
+    switch(selected){
+      case 1:
+        System.out.println("検索します。");
+        break;
+      case 2:
+        System.out.println("登録します。");
+        break;
+      case 3:
+       System.out.println("削除します。");
+       break;
+      case 4:
+      System.out.println("変更します。");
+      break;
+      // default:
+      //   System.out.println("");   何も表示しないので表記不要
+    }    
+
+  }
+}
