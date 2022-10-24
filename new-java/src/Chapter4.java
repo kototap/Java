cd490cbddcd451193b9ec9c09de8783d14ff5b56
Kototap <kotonesmz31@gmail.com>
2022-10-24 23:56:35 +0900

    Chapter4-4-3

--- new-java/src/Chapter4.java
+++ new-java/src/Chapter4.java
@@ -7,7 +7,11 @@ public class Chapter4 {
 
   public static void main(String[] args){
     int[] scores = {20, 30, 40, 50, 80, 100};
-    int sum = scores[0] + scores[1] + scores[2] + scores[3] + scores[4] + scores[5];
+    int sum = 0;
+    for(int i = 0; i < scores.length; i++){
+      sum += scores[i];
+    }
+    // int sum = scores[0] + scores[1] + scores[2] + scores[3] + scores[4] + scores[5];
     int ave = sum / scores.length;
     System.out.println("合計点は" + sum + "点です。");
     System.out.println(scores.length + "教科の平均点は" + ave + "点です。");
