07c685441c1fd046a1d2fdbc255f3c5b06cbf081
Kototap <kotonesmz31@gmail.com>
2022-10-24 23:59:37 +0900

    4-4-4

--- new-java/src/Chapter4.java
+++ new-java/src/Chapter4.java
@@ -5,15 +5,26 @@ public class Chapter4 {
   //   System.out.println("要素の数は" + num + "です。");
   // } 
 
+  // public static void main(String[] args){
+  //   int[] scores = {20, 30, 40, 50, 80, 100};
+  //   int sum = 0;
+  //   for(int i = 0; i < scores.length; i++){
+  //     sum += scores[i];
+  //   }
+  //   // int sum = scores[0] + scores[1] + scores[2] + scores[3] + scores[4] + scores[5];
+  //   int ave = sum / scores.length;
+  //   System.out.println("合計点は" + sum + "点です。");
+  //   System.out.println(scores.length + "教科の平均点は" + ave + "点です。");
+  // }
+
   public static void main(String[] args){
     int[] scores = {20, 30, 40, 50, 80, 100};
-    int sum = 0;
+    int count = 0;
     for(int i = 0; i < scores.length; i++){
-      sum += scores[i];
+      if(scores[i] >= 50){
+        count++;
+      }
     }
-    // int sum = scores[0] + scores[1] + scores[2] + scores[3] + scores[4] + scores[5];
-    int ave = sum / scores.length;
-    System.out.println("合計点は" + sum + "点です。");
-    System.out.println(scores.length + "教科の平均点は" + ave + "点です。");
+    System.out.println("50点以上の科目数は" + count);
   }
 }
