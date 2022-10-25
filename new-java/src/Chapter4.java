3627a7252b959794dc13a706cb77520bfe21240f
Kototap <kotonesmz31@gmail.com>
2022-10-25 23:11:05 +0900

    練習問題4-2

--- new-java/src/Chapter4.java
+++ new-java/src/Chapter4.java
@@ -61,10 +61,21 @@ public class Chapter4 {
   //練習問題
 
 //4-1
+  // public static void main(String[] args){
+  //   int[] points = new int[4];
+  //   double[] weights = new double[5];
+  //   boolean[] answers = new boolean[3];
+  //   String[] names = new String[3];
+  // }
+
+  //4-2
   public static void main(String[] args){
-    int[] points = new int[4];
-    double[] weights = new double[5];
-    boolean[] answers = new boolean[3];
-    String[] names = new String[3];
+    int[] moneyList = {121902, 8302, 55100};
+    for(int i = 0; i < moneyList.length; i++){
+      System.out.println(moneyList[i]);
+    }
+    for(int value : moneyList){
+      System.out.println(value);
+    }
   }
 } 
