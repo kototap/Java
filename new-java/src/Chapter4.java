2ad0ba5afe292a5d1086070f9fc964f4c22aaabe
Kototap <kotonesmz31@gmail.com>
2022-10-25 23:28:28 +0900

    練習問題4-4

--- new-java/src/Chapter4.java
+++ new-java/src/Chapter4.java
@@ -69,13 +69,26 @@ public class Chapter4 {
   // }
 
   //4-2
+  // public static void main(String[] args){
+  //   int[] moneyList = {121902, 8302, 55100};
+  //   for(int i = 0; i < moneyList.length; i++){
+  //     System.out.println(moneyList[i]);
+  //   }
+  //   for(int value : moneyList){
+  //     System.out.println(value);
+  //   }
+  // }
+
+
+  //4-4
   public static void main(String[] args){
-    int[] moneyList = {121902, 8302, 55100};
-    for(int i = 0; i < moneyList.length; i++){
-      System.out.println(moneyList[i]);
-    }
-    for(int value : moneyList){
-      System.out.println(value);
+    int[] numbers = {3, 4, 9};
+    System.out.println("1桁の数字を入力してください。");
+    int input = new java.util.Scanner(System.in).nextInt();
+    for(int n : numbers){
+      if(n == input){
+        System.out.println("あたり！");
+      } 
     }
   }
 } 
