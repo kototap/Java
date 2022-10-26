3e48e2350c8827616cd27b7684709fafaa39299f
Kototap <kotonesmz31@gmail.com>
2022-10-26 14:36:25 +0900

    コード5-2

---
+++ new-java/src/Chapter5.java
@@ -0,0 +1,11 @@
+public class Chapter5 {
+  public static void main(String[] args){
+    System.out.println("メソッドを呼び出します。");
+    hello();    //helloメソッドの呼び出し
+    System.out.println("メソッドを呼び出しました。");
+  }
+  //メソッドの本体
+  public static void hello(){
+    System.out.println("こんにちは！");
+  }
+}
