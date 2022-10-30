d775e64865dbae7573eb8b55038f086cb220ba0c
Kototap <kotonesmz31@gmail.com>
2022-10-30 13:41:31 +0900

    コード5-5

--- new-java/src/Chapter5.java
+++ new-java/src/Chapter5.java
@@ -26,15 +26,26 @@ public class Chapter5 {
 
 
   //コード5-4 引数の利用
-    public static void main(String[] args){
-      System.out.println("メソッドを呼び出します");
-      hello("京本");
-      hello("森本");
-      hello("田中");
-      System.out.println("メソッドの呼び出し終了");
-    }
+    // public static void main(String[] args){
+    //   System.out.println("メソッドを呼び出します");
+    //   hello("京本");
+    //   hello("森本");
+    //   hello("田中");
+    //   System.out.println("メソッドの呼び出し終了");
+    // }
 
-    public static void hello(String name){    //String nameで文字型変数nameを宣言
-      System.out.println(name + "さんこんにちは！");
+    // public static void hello(String name){    //String nameで文字型変数nameを宣言
+    //   System.out.println(name + "さんこんにちは！");
+    // }
+
+
+    //コード5-5
+    public static void main(String[] args){
+      add(100, 200);
+      add(300, 50);
+    }
+    public static void add(int x, int y){
+      int ans = x + y;
+      System.out.println(ans);
     }
 }
