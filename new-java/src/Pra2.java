0871ed19bbb3b3279899ff89f1c8ef8c2c7a9fa1
Kototap <kotonesmz31@gmail.com>
2022-11-06 12:31:02 +0900

    4問目

--- new-java/src/Pra2.java
+++ new-java/src/Pra2.java
@@ -26,19 +26,39 @@ public class Pra2 {
 // - 3問目
 // 1〜10までの整数をカンマ区切りで横並びで表示させる
 // (10にはカンマをつけない)
-public static void main(String[] args){
-  for(int i = 1; i < 11; i++){
-    if(i < 10){
-      System.out.print(i + ",");
-    } else {
-      System.out.println(i);
-    }
+// public static void main(String[] args){
+//   for(int i = 1; i < 11; i++){
+//     if(i < 10){
+//       System.out.print(i + ",");
+//     } else {
+//       System.out.print(i);
+//     }
     
-  }
-}
+//   }
+// }
 
 
 // - 4問目
 // 要素数20の配列を宣言し、要素に0から5ずつ加算して代入させる。[0]~[10]の値の
 // 奇数のみカンマ区切りで、[11]~[19]の値は偶数をカンマ区切りで表示させる
+public static void main(String[] args){
+  int num[] = new int[20];
+  num[0] = 0;
+  for(int i = 0; i < num.length; i++){
+    if(i == 0){
+      num[i] += 5;
+    } else{
+      num[i] = num[i - 1] + 5;
+    }
+    if(i < 11 && num[i] % 2 ==1){
+      System.out.print(num[i] + ",");
+    } else if(i > 11 && num[i] % 2 == 0){
+      System.out.print(num[i] + ",");
+    } else {
+      System.out.print(num[i] + " ");
+    }
+  }
+}
+
+
 }
