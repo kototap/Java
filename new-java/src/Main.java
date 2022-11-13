b1a06833c09a6d69f592d704451ef63d356f632c
Kototap <kotonesmz31@gmail.com>
2022-11-13 16:49:58 +0900

    8-12

--- new-java/src/Main.java
+++ new-java/src/Main.java
@@ -2,5 +2,10 @@ public class Main {
   public static void main(String[] args){
     //勇者を生成
     Hero h = new Hero();
+
+    //フィールドに初期値を設定(変数名.フィールド名 = 値;)
+    h.name = "ことね";
+    h.hp = 100;
+    System.out.println("勇者" + h.name + "を生み出しました！");
   }
 }
